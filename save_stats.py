a2d83ce273ef4dc0acacf90b96b4b47db61cc3a5
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2011-05-27 16:55:10 +0200

    limited file to 80 columns

--- save_stats.py
+++ save_stats.py
@@ -5,12 +5,15 @@ import config
 
 ADL.SetupADL(config.device_number)
 
+config.
 
 for i in range(config.update_frequency):
     conn = sqlite3.connect(config.db_file)
     c = conn.cursor()
 
-    c.execute("create table if not exists stats (time REAL, load INTEGER, temp REAL, fanspeed INTEGER, fanrpm INTEGER, hashrate REAL, efficiency REAL)")
+    c.execute("create table if not exists stats (time REAL, load INTEGER, "
+              "temp REAL, fanspeed INTEGER, fanrpm INTEGER, hashrate REAL, "
+              "efficiency REAL)")
 
     try:
         hashrate, efficiency = open(config.miner_log).readline().split(':')
